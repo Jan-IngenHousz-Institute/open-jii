7aee1562827681ef73c9d2cc309bf35a3809595c
petar-INFO <petar@info.nl>
2025-02-05 15:16:32 +0100

    feat(terraform): sets up infrastructure baseline for AWS

--- infrastructure/env/dev/backend.tf
+++ infrastructure/env/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "open-jii-terraform-state"
     key            = "terraform.tfstate"
-    region         = var.aws_region
+    region         = "eu-central-1"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
   }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,11 +1,5 @@
-variable "environment" {
-  type        = string
-  description = "Environment name"
-  default = "dev"
-}
-
 variable "aws_region" {
+  description = "AWS region for infrastructure deployment"
   type        = string
-  description = "AWS region"
   default     = "eu-central-1"
 }
