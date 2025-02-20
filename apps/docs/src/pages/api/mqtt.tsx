432ddbb592353d247347844ca9df1600937b77bb
petar-INFO <petar@info.nl>
2025-02-20 18:08:20 +0100

    chore: change api build file path

--- apps/docs/src/pages/api/mqtt.tsx
+++ apps/docs/src/pages/api/mqtt.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import Layout from '@theme/Layout';
+import Layout from "@theme/Layout";
+import React from "react";
 
 export default function MQTTApiPage(): JSX.Element {
   return (
-    <Layout
-      title="MQTT API Documentation"
-    >
-      <div style={{ width: '100%', height: '800px' }}>
+    <Layout title="MQTT API Documentation">
+      <div style={{ width: "100%", height: "800px" }}>
         <iframe
-          src="/asyncapi/index.html"
+          src="/.asyncapi/index.html"
           title="API Documentation"
           style={{
-            width: '100%',
-            height: '100%',
-            border: 'none',
-            display: 'block'
+            width: "100%",
+            height: "100%",
+            border: "none",
+            display: "block",
           }}
         />
       </div>
--- apps/docs/src/pages/api/rest.tsx
+++ apps/docs/src/pages/api/rest.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import Layout from '@theme/Layout';
+import Layout from "@theme/Layout";
+import React from "react";
 
 export default function RESTApiPage(): JSX.Element {
   return (
-    <Layout
-      title="REST API Documentation"
-    >
-      <div style={{ width: '100%', height: '800px' }}>
+    <Layout title="REST API Documentation">
+      <div style={{ width: "100%", height: "800px" }}>
         <iframe
-          src="/openapi/index.html"
+          src="/.openapi/index.html"
           title="API Documentation"
           style={{
-            width: '100%',
-            height: '100%',
-            border: 'none',
-            display: 'block'
+            width: "100%",
+            height: "100%",
+            border: "none",
+            display: "block",
           }}
         />
       </div>
