6435df40c66fe2e551fa7d5a01237dffbadd8b00
petar-INFO <petar@info.nl>
2025-03-06 20:03:51 +0100

    feat(databricks): introduces data ingest & transform pipelines and orchestrating job

--- infrastructure/modules/databricks/jobs/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "job_id" {
-  description = "ID of the created Databricks job"
-  value       = databricks_job.ingest_pipeline.id
-}
-
-output "job_url" {
-  description = "URL to access the job in the Databricks workspace"
-  value       = databricks_job.ingest_pipeline.url
-}
