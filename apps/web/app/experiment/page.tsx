176e8369ffc5cbc28c6c518f80ee265ad253489a
Michiel Schutler <107392628+michielschutler@users.noreply.github.com>
2025-04-29 09:32:16 +0200

    feat(web): basic layout (#183)
    ## Type of PR (check all applicable)
    
    - [X] Feature
    - [ ] Bug Fix
    - [ ] Refactor
    - [ ] Documentation Update
    - [ ] Performance Improvement
    - [ ] Test Update
    - [ ] Other (please describe)
    
    ## Description
    
    Basic layout setup for web application.

---
+++ apps/web/app/experiment/page.tsx
@@ -0,0 +1,8 @@
+export default function ExperimentPage() {
+  return (
+    <main className="pl-20 pr-20 pt-8">
+      <h1>Experiments</h1>
+      <p>This page will show the list of experiments.</p>
+  </main>
+  );
+}
