3568d3e7430b72e4705bf5e09ea0c52a79a86cbf
Dominik Vrbic <dominik@info.nl>
2025-04-29 17:15:50 +0200

    feat: enhance project structure with Tailwind CSS integration and API contracts

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+
+/** @type {import("next").NextConfig} */
+const config = {
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: [
+
+    "@repo/ui",
+    "@repo/validator",
+  ],
+
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
+
+export default config;
