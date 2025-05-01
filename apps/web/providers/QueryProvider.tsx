2f9fe30327b70e94a25694ee078e1c9380b9d42b
petar-INFO <petar@info.nl>
2025-05-01 09:11:26 +0200

    feat(web): adds react-query-devtools

--- apps/web/providers/QueryProvider.tsx
+++ apps/web/providers/QueryProvider.tsx
@@ -3,6 +3,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { tsr } from "../lib/tsr";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // Create a client-side QueryClient instance
 const queryClient = new QueryClient();
@@ -10,7 +11,10 @@ const queryClient = new QueryClient();
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <tsr.ReactQueryProvider>{children}</tsr.ReactQueryProvider>
+      <tsr.ReactQueryProvider>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </tsr.ReactQueryProvider>
     </QueryClientProvider>
   );
 }
