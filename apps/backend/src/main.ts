32bdf53fd0ca54b11a06f84fabbc1b24d23ef6db
petar-INFO <petar@info.nl>
2025-05-14 15:08:48 +0200

    feat: enables cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,7 +3,19 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  const corsEnabled = process.env.CORS_ENABLED !== "false";
+  if (corsEnabled) {
+    const corsOrigins = process.env.CORS_ORIGINS
+      ? process.env.CORS_ORIGINS.split(",").map((origin) => origin.trim())
+      : ["http://localhost:3000"]; // Default origin
+
+    app.enableCors({
+      origin: corsOrigins,
+      credentials: true,
+    });
+  }
 
   await app.listen(process.env.PORT ?? 3020);
 }
