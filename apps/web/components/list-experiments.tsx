700791705c03a049a24cbf6bb43f963092ddcc44
Michiel Schutler <michiel@info.nl>
2025-05-19 16:59:10 +0200

    feat(web): List experiments

---
+++ apps/web/components/list-experiments.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useExperiments } from "../hooks/experiment/useExperiments/useExperiments";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@repo/ui/components";
+import {
+  EditIcon,
+  LockIcon,
+} from "lucide-react";
+import { zExperimentVisibility } from "@repo/api";
+import Link from "next/link";
+
+export function ListExperiments() {
+  // In a real app, you would get this from authentication context
+  const userId = "00000000-0000-0000-0000-000000000000";
+
+  const { data } = useExperiments(userId);
+
+  if (data) {
+    return (
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Experiment name</TableHead>
+            <TableHead>Private</TableHead>
+            <TableHead>Created</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.body.map((experiment) => {
+            console.log(experiment);
+            return (
+              <TableRow key={experiment.id}>
+                <TableCell>{experiment.name}</TableCell>
+                <TableCell>{experiment.visibility === zExperimentVisibility.enum.private && <LockIcon size={18} />}</TableCell>
+                <TableCell>{experiment.createdAt.substring(0,10)}</TableCell>
+                <TableCell><Link href={`/openjii/experiments/${experiment.id}`}><EditIcon size={18} /></Link></TableCell>
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+    )
+  }
+}
