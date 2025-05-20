d80286a7858c798f592a343652a3934a2732002e
Michiel Schutler <michiel@info.nl>
2025-05-20 13:25:22 +0200

    feat(web): Fixed schema

--- apps/web/util/schema.ts
+++ apps/web/util/schema.ts
@@ -1,14 +1,14 @@
 import z from "zod";
 
-import { zExperimentVisibility } from "@repo/api";
+import { zUpdateExperimentBody } from "@repo/api";
 
-export const editExperimentFormSchema = z.object({
-  id: z.string(),
-  name: z.string().min(1).max(100),
-  description: z.string(),
-  visibility: zExperimentVisibility,
-  embargoIntervalDays: z.number(),
-});
+export const editExperimentFormSchema = zUpdateExperimentBody
+  .required({
+    name: true,
+  })
+  .extend({
+    id: z.string().uuid(),
+  });
 
 export type EditExperimentForm = z.infer<typeof editExperimentFormSchema>;
 
