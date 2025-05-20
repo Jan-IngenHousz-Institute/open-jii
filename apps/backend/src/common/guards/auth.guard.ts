2bae3830942bf8a77276e34b1e25410f5030809a
petar-INFO <petar@info.nl>
2025-05-20 11:50:51 +0200

    chore: unit tests auth guard

--- apps/backend/src/common/guards/auth.guard.ts
+++ apps/backend/src/common/guards/auth.guard.ts
@@ -13,27 +13,11 @@ export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<Request>();
     const session = await getSession(request);
+
     if (!session) {
       throw new UnauthorizedException("Unauthorized");
     }
 
-    // if (!authHeader) {
-    //   throw new UnauthorizedException("Authorization header is missing");
-    // }
-
-    // // Check if it's a Bearer token
-    // if (!authHeader.startsWith("Bearer ")) {
-    //   throw new UnauthorizedException("Invalid token format");
-    // }
-
-    // // Validate the token
-    // const session = await validateToken(authHeader);
-
-    // if (!session) {
-    //   throw new UnauthorizedException("Invalid or expired token");
-    // }
-
-    // Add the user to the request object so it can be accessed in controllers
     request["user"] = session.user;
 
     return true;
