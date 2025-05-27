a46ec990838a13a2644a2168ae22e5370dcbf160
Ivo Katunaric <ikatun@gmail.com>
2025-05-27 12:24:52 +0100

    Restore restrictEnvAccess

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -7,6 +7,36 @@ import turboPlugin from "eslint-plugin-turbo";
 import prettierPlugin from "eslint-plugin-prettier";
 import { includeIgnoreFile } from "@eslint/compat";
 
+/**
+ * All packages that leverage t3-env should use this rule
+ */
+export const restrictEnvAccess = tseslint.config(
+  { ignores: ["**/env.ts"] },
+  {
+    files: ["**/*.js", "**/*.ts", "**/*.tsx"],
+    rules: {
+      "no-restricted-properties": [
+        "error",
+        {
+          object: "process",
+          property: "env",
+          message:
+            "Use `import { env } from '~/env'` instead to ensure validated types.",
+        },
+      ],
+      "no-restricted-imports": [
+        "error",
+        {
+          name: "process",
+          importNames: ["env"],
+          message:
+            "Use `import { env } from '~/env'` instead to ensure validated types.",
+        },
+      ],
+    },
+  },
+);
+
 export default tseslint.config(
   includeIgnoreFile(path.join(import.meta.dirname, "../../.gitignore")),
 
