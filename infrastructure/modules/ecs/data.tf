29b6df5962d31c7ac79751ca176c9f26ec81293b
Svetoslav Gerganov <svetoslav@info.nl>
2025-05-30 12:46:08 +0300

    add small data res.

--- infrastructure/modules/ecs/data.tf
+++ infrastructure/modules/ecs/data.tf
@@ -1,3 +1,7 @@
 data "aws_ecr_repository" "existing_repo" {
   name = "open-jii" # Use the exact name of your manually created repo
 }
+
+# data "aws_rds_cluster" "aurora" {
+#  cluster_identifier = "your-aurora-cluster-id"
+#}
