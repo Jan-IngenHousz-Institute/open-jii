445e1af4a7f06fa52b715621e412814043ff26f0
Ivo Katunaric <ikatun@gmail.com>
2025-06-03 13:20:26 +0100

    Got auth fully working locally

--- apps/mobile/src/screens/login-screen/login-screen.tsx
+++ apps/mobile/src/screens/login-screen/login-screen.tsx
@@ -1,25 +1,22 @@
+import axios from "axios";
 import { createURL, parse } from "expo-linking";
 import { openAuthSessionAsync } from "expo-web-browser";
-import { useAsync, useAsyncCallback } from "react-async-hook";
+import { useAsyncCallback } from "react-async-hook";
 import { View } from "react-native";
 import { BigActionButton } from "~/components/big-action-button";
 import { assertEnvVariables } from "~/utils/assert";
 
-const { NEXT_AUTH_URI } = assertEnvVariables({
+const { NEXT_AUTH_URI, NEXT_REDIRECT_URI } = assertEnvVariables({
   NEXT_AUTH_URI: process.env.NEXT_AUTH_URI,
+  NEXT_REDIRECT_URI: process.env.NEXT_REDIRECT_URI,
 });
 
 console.log("NEXT_AUTH_URI", NEXT_AUTH_URI);
 
 async function signIn() {
   const signInUrl = `${NEXT_AUTH_URI}/api/auth/signin`;
-  const redirectTo = createURL("/login");
-  console.log("redirectTo", redirectTo);
-  const loginUrl = `${signInUrl}?expo-redirect=${encodeURIComponent(redirectTo)}`;
-  console.log("loginUrl", loginUrl);
-  const result = await openAuthSessionAsync(loginUrl, redirectTo);
-
-  console.log("result", result);
+  const loginUrl = `${signInUrl}?callbackUrl=${encodeURIComponent(NEXT_REDIRECT_URI)}`;
+  const result = await openAuthSessionAsync(loginUrl, createURL("/callback"));
 
   if (result.type !== "success") {
     return undefined;
@@ -37,6 +34,17 @@ export function LoginScreen() {
   const { execute: startLoginFlow } = useAsyncCallback(async () => {
     const token = await signIn();
     console.log("token", token);
+    const response = await axios.get(
+      "http://localhost:3020/api/v1/users/search",
+      {
+        headers: {
+          Accept: "*/*",
+          Cookie: `authjs.session-token=${token}`,
+        },
+      },
+    );
+
+    console.log(response.data);
   });
 
   return (
---
+++ apps/web/app/api/auth/mobile-redirect/route.ts
@@ -0,0 +1,15 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function GET(request: NextRequest) {
+  const sessionToken = request.cookies.get("authjs.session-token")?.value;
+
+  if (!sessionToken) {
+    return new NextResponse("Invalid request: missing session token", {
+      status: 400,
+    });
+  }
+
+  const redirectUrl = `photosynq://callback?session_token=${encodeURIComponent(sessionToken)}`;
+  return NextResponse.redirect(redirectUrl);
+}
