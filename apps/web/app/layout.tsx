5af20ab6c2efa120e4f45789edc603980c974961
Dominik Vrbic <dominik@info.nl>
2025-06-10 17:22:36 +0200

    refactor: simplify RootLayout component by removing unused imports and unnecessary HTML structure

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,23 +1,7 @@
 import type { Metadata } from "next";
-import { Poppins, Overpass } from "next/font/google";
 import type React from "react";
 
-import { cn } from "@repo/ui/lib/utils";
-
-import { QueryProvider } from "../providers/QueryProvider";
 import "./globals.css";
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["600", "700"],
-  variable: "--font-poppins",
-});
-
-const overpass = Overpass({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"],
-  variable: "--font-overpass",
-});
 
 export const metadata: Metadata = {
   title: "Jan IngenHousz Institute",
@@ -29,17 +13,5 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html>
-      <body
-        className={cn(
-          "bg-background font-overpass min-h-screen antialiased",
-          poppins.variable,
-          overpass.variable,
-        )}
-      >
-        <QueryProvider>{children}</QueryProvider>
-      </body>
-    </html>
-  );
+  return children;
 }
