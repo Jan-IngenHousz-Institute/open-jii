07677f003c025d74a564a075ec27d2e1e4dd3438
Ivo Katunaric <ikatun@gmail.com>
2025-06-25 21:41:34 +0100

    Use env route for backend

--- apps/mobile/src/api/tsr.ts
+++ apps/mobile/src/api/tsr.ts
@@ -2,8 +2,11 @@ import { tsRestFetchApi } from "@ts-rest/core";
 import type { ApiFetcherArgs } from "@ts-rest/core";
 import { initTsrReactQuery } from "@ts-rest/react-query/v5";
 import { useSessionStore } from "~/hooks/use-session-store";
+import { assertEnvVariables } from "~/utils/assert";
 
 import { contract } from "@repo/api";
+
+const { BACKEND_URI } = assertEnvVariables({ BACKEND_URI: process.env.BACKEND_URI });
 
 const customApiFetcher = async (args: ApiFetcherArgs) => {
   const token = useSessionStore.getState().session?.token;
@@ -20,7 +23,7 @@ const customApiFetcher = async (args: ApiFetcherArgs) => {
 };
 
 export const tsr = initTsrReactQuery(contract, {
-  baseUrl: "http://localhost:3020",
+  baseUrl: BACKEND_URI,
   baseHeaders: {
     "x-app-source": "ts-rest",
   },
