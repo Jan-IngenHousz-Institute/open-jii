24ca5b75c94dc53310bc1e7e3b2de8876906c4b3
petar-INFO <petar@info.nl>
2025-08-04 15:07:32 +0200

    feat

--- infrastructure/modules/databricks/secret_scope/outputs.tf
+++ infrastructure/modules/databricks/secret_scope/outputs.tf
@@ -9,9 +9,3 @@ output "scope_id" {
   value       = databricks_secret_scope.this.id
   sensitive   = true
 }
-
-output "secret_keys" {
-  description = "Map of secret names to their keys, allowing access like secret_keys[\"key_name\"]"
-  value       = { for k, v in var.secrets : k => k }
-  sensitive   = false
-}
