1124e1ca85c3d4a5fa844f123000a1092bc0c7c0
petar-INFO <petar@info.nl>
2025-08-12 17:46:50 +0200

    copilot suggestion shrug

--- infrastructure/modules/ecs/variables.tf
+++ infrastructure/modules/ecs/variables.tf
@@ -207,7 +207,7 @@ variable "enable_container_healthcheck" {
 variable "health_check_path" {
   description = "The path for container health check endpoint"
   type        = string
-  default     = null
+  default     = "/health"
 }
 
 variable "enable_circuit_breaker" {
