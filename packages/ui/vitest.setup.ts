f969f0f1abc8a5d4efe33ac09a1ac70aee76f29e
petar-INFO <petar@info.nl>
2025-08-27 15:24:03 +0200

    janitorial work

--- packages/ui/vitest.setup.ts
+++ packages/ui/vitest.setup.ts
@@ -2,7 +2,6 @@ import * as matchers from "@testing-library/jest-dom/matchers";
 import { cleanup } from "@testing-library/react";
 import { expect, afterEach } from "vitest";
 
-// Extend Vitest expect with jest-dom matchers
 expect.extend(matchers);
 
 afterEach(() => {
