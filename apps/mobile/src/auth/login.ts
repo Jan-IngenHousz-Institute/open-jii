7e1e682cbd1b4c5b6afc6decf62bba1a057adb55
Ivo Katunaric <ikatun@gmail.com>
2025-10-06 19:20:36 +0200

    Default to chrome, allow non-chrome

--- apps/mobile/src/auth/login.ts
+++ apps/mobile/src/auth/login.ts
@@ -1,19 +1,50 @@
 import { parse } from "expo-linking";
-import { openAuthSessionAsync } from "expo-web-browser";
+import { getCustomTabsSupportingBrowsersAsync, openAuthSessionAsync } from "expo-web-browser";
+import Toast from "react-native-toast-message";
 import { getLoginArgs } from "~/api/get-login-args";
+import { delay } from "~/utils/delay";
+
+async function getBrowserPackage(): Promise<string | undefined> {
+  try {
+    const { preferredBrowserPackage, browserPackages } =
+      await getCustomTabsSupportingBrowsersAsync();
+
+    if (browserPackages.includes("com.android.chrome")) {
+      return "com.android.chrome";
+    }
+
+    Toast.show({
+      text1: "Chrome is browser is recommended for safer login flow",
+      type: "error",
+    });
+    await delay(3000);
+    return preferredBrowserPackage;
+  } catch {
+    Toast.show({
+      text1: "Chrome is browser is recommended for safer login flow",
+      type: "error",
+    });
+    await delay(3000);
+    return undefined;
+  }
+}
 
 export async function login() {
   const { expectedRedirectUrl, loginUrl } = getLoginArgs();
 
+  const browserPackage = await getBrowserPackage();
+
   const result = await openAuthSessionAsync(loginUrl, expectedRedirectUrl, {
-    browserPackage: "com.android.chrome",
+    browserPackage,
   });
 
   if (result.type !== "success") {
     return undefined;
   }
+
   const url = parse(result.url);
   const sessionToken = String(url.queryParams?.session_token);
+
   if (!sessionToken) {
     throw new Error("No session token found");
   }
