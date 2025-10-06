1800d704d0f98407efd134cef0fee184ec8190ea
Ivo Katunaric <ikatun@gmail.com>
2025-10-06 20:25:42 +0200

    Fix chrome login check

--- apps/mobile/src/auth/login.ts
+++ apps/mobile/src/auth/login.ts
@@ -1,37 +1,30 @@
 import { parse } from "expo-linking";
-import { getCustomTabsSupportingBrowsersAsync, openAuthSessionAsync } from "expo-web-browser";
+import * as Linking from "expo-linking";
+import { openAuthSessionAsync } from "expo-web-browser";
 import Toast from "react-native-toast-message";
 import { getLoginArgs } from "~/api/get-login-args";
 import { delay } from "~/utils/delay";
 
 async function getBrowserPackage(): Promise<string | undefined> {
   try {
-    const { preferredBrowserPackage, browserPackages } =
-      await getCustomTabsSupportingBrowsersAsync();
-
-    if (browserPackages.includes("com.android.chrome")) {
+    // Check if Chrome package URL scheme can be opened
+    if (await Linking.canOpenURL("googlechrome://")) {
       return "com.android.chrome";
     }
-
-    Toast.show({
-      text1: "Chrome browser is recommended for safer login flow",
-      type: "error",
-    });
-    await delay(3000);
-    return preferredBrowserPackage;
   } catch {
-    Toast.show({
-      text1: "Chrome browser is recommended for safer login flow",
-      type: "error",
-    });
-    await delay(3000);
-    return undefined;
+    // ignored
   }
+
+  Toast.show({
+    text1: "Chrome browser is recommended for safer login flow",
+    type: "error",
+  });
+  await delay(3000);
+  return undefined; // fallback to system default
 }
 
 export async function login() {
   const { expectedRedirectUrl, loginUrl } = getLoginArgs();
-
   const browserPackage = await getBrowserPackage();
 
   const result = await openAuthSessionAsync(loginUrl, expectedRedirectUrl, {
