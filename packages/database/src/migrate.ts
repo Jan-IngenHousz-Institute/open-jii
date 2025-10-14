6b210da47563ee4714fac96a37a6678abeb78735
petar-INFO <petar@info.nl>
2025-10-14 16:06:26 +0200

    fix(db): fixes exit process in migration script

--- packages/database/src/migrate.ts
+++ packages/database/src/migrate.ts
@@ -4,16 +4,19 @@ import { migrate } from "drizzle-orm/postgres-js/migrator";
 import { getClient, db } from "./database";
 
 async function runMigrations() {
+  let exit = 0;
+  
   try {
     // Run the migrations
     await migrate(db, { migrationsFolder: "drizzle" });
     console.log("Migrations completed successfully");
   } catch (error) {
     console.error("Error running migrations:", error);
-    process.exit(1);
+    exit = 1;
   } finally {
     // Close the postgres client
     await getClient().end();
+    process.exit(exit);
   }
 }
 
