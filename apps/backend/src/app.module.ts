b56e1a1f4d0d5fa0efcfa9bb974c7073025c4cda
petar-INFO <petar@info.nl>
2025-10-30 12:19:29 +0100

    work

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -10,6 +10,7 @@ import analyticsConfig from "./common/config/analytics.config";
 import awsConfig from "./common/config/aws.config";
 import databaseConfig from "./common/config/database.config";
 import databricksConfig from "./common/config/databricks.config";
+import deltaConfig from "./common/config/delta.config";
 import emailConfig from "./common/config/email.config";
 import { DatabaseModule } from "./common/database/database.module";
 import { AnalyticsModule } from "./common/modules/analytics/analytics.module";
@@ -23,7 +24,14 @@ import { UserModule } from "./users/user.module";
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [databaseConfig, databricksConfig, awsConfig, emailConfig, analyticsConfig],
+      load: [
+        databaseConfig,
+        databricksConfig,
+        awsConfig,
+        emailConfig,
+        analyticsConfig,
+        deltaConfig,
+      ],
     }),
     ScheduleModule.forRoot(),
     BetterAuthModule.forRoot({ auth }),
