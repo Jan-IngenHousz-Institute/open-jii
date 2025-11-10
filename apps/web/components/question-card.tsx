2aad89bcfb0196c32af6f7ba8b59ee7860749a94
Dominik Vrbic <dominik@info.nl>
2025-11-10 10:50:30 +0100

    refactor: remove deprecated QuestionCard re-exports following modular structure update

--- apps/web/components/question-card.tsx
+++
@@ -1,3 +0,0 @@
-// Re-export from the new modular structure in the question-card folder
-export { QuestionCard } from "./question-card/question-card";
-export type { QuestionUI } from "./question-card/question-card";
