012b3ff027dbd1d54078d0f9a5db01cf19c3e0cd
Ivo Katunaric <ikatun@gmail.com>
2025-11-21 00:07:17 +0100

    Prepare to move useScannerCommandExecutor to context

---
+++ apps/mobile/src/services/scan-manager/utils/create-serial-port-command-executor.ts
@@ -0,0 +1,12 @@
+import { getConnectedSerialPortConnection } from "~/services/device-connection-manager/device-connection-manager";
+import { serialPortToMultispeqStream } from "~/services/multispeq-communication/android-serial-port-connection/serial-port-to-multispeq-stream";
+import { MultispeqCommandExecutor } from "~/services/multispeq-communication/multispeq-command-executor";
+
+export function createSerialPortCommandExecutor() {
+  const connection = getConnectedSerialPortConnection();
+  if (!connection) {
+    return undefined;
+  }
+
+  return new MultispeqCommandExecutor(serialPortToMultispeqStream(connection));
+}
