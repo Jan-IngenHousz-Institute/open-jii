592457b492f8eb02b7139af4f3704d1b7699a61d
Ivo Katunaric <ikatun@gmail.com>
2025-11-21 00:27:31 +0100

    Introduce device management store

--- apps/mobile/src/services/scan-manager/use-scanner-command-executor.ts
+++ apps/mobile/src/services/scan-manager/use-scanner-command-executor.ts
@@ -1,21 +1,8 @@
-import { useAsync, useAsyncCallback } from "react-async-hook";
-import { useConnectedDevice } from "~/services/device-connection-manager/device-connection-manager";
-import { createMultispeqCommandExecutor } from "~/services/scan-manager/utils/create-multispeq-command-executor";
+import { useScannerCommandExecutorStore } from "~/stores/use-scanner-command-executor-store";
 
 export function useScannerCommandExecutor() {
-  const { data: device } = useConnectedDevice();
-  const { result: commandExecutor } = useAsync(
-    () => createMultispeqCommandExecutor(device ?? undefined),
-    [device?.id],
-  );
-
-  const {
-    result: commandResponse,
-    reset,
-    loading: isExecuting,
-    error,
-    execute: executeCommand,
-  } = useAsyncCallback((command: string | object) => commandExecutor?.execute(command));
+  const { commandResponse, reset, isExecuting, error, executeCommand } =
+    useScannerCommandExecutorStore();
 
   return {
     commandResponse,
