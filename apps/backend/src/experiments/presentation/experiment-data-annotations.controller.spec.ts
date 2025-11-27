8095633dbd16ddeb58eddaddb1e3fa3537562ba2
Michiel Schutler <michiel@info.nl>
2025-11-27 11:50:46 +0100

    backend(web): WIP

--- apps/backend/src/experiments/presentation/experiment-data-annotations.controller.spec.ts
+++ apps/backend/src/experiments/presentation/experiment-data-annotations.controller.spec.ts
@@ -1,17 +1,27 @@
 import { StatusCodes } from "http-status-codes";
 
-import type { AddAnnotationBody, AnnotationRowsAffected } from "@repo/api";
+import type {
+  AddAnnotationBody,
+  AddAnnotationsBulkBody,
+  AnnotationRowsAffected,
+  DeleteAnnotationsBulkBody,
+  UpdateAnnotationBody,
+} from "@repo/api";
 import { contract } from "@repo/api";
 
 import { success } from "../../common/utils/fp-utils";
 import { TestHarness } from "../../test/test-harness";
 import type { SuperTestResponse } from "../../test/test-harness";
 import { AddAnnotationsUseCase } from "../application/use-cases/experiment-data-annotations/add-annotations/add-annotations";
+import { DeleteAnnotationsUseCase } from "../application/use-cases/experiment-data-annotations/delete-annotations/delete-annotations";
+import { UpdateAnnotationUseCase } from "../application/use-cases/experiment-data-annotations/update-annotation/update-annotation";
 
 describe("ExperimentDataAnnotationsController", () => {
   const testApp = TestHarness.App;
   let testUserId: string;
   let addAnnotationsUseCase: AddAnnotationsUseCase;
+  let updateAnnotationUseCase: UpdateAnnotationUseCase;
+  let deleteAnnotationsUseCase: DeleteAnnotationsUseCase;
 
   beforeAll(async () => {
     await testApp.setup();
@@ -22,6 +32,8 @@ describe("ExperimentDataAnnotationsController", () => {
     testUserId = await testApp.createTestUser({});
 
     addAnnotationsUseCase = testApp.module.get(AddAnnotationsUseCase);
+    updateAnnotationUseCase = testApp.module.get(UpdateAnnotationUseCase);
+    deleteAnnotationsUseCase = testApp.module.get(DeleteAnnotationsUseCase);
 
     // Reset any mocks before each test
     vi.restoreAllMocks();
@@ -41,11 +53,11 @@ describe("ExperimentDataAnnotationsController", () => {
 
       // Construct the path
       const path = testApp.resolvePath(contract.experiments.addAnnotation.path, {
-        id: "experiment_123",
+        id: "06a9ac24-b888-4a97-a883-16354d2cf63c",
       });
 
       // Create the request body
-      const addAnnotationData: AddAnnotationBody = {
+      const addAnnotationsData: AddAnnotationBody = {
         tableName: "data_table_1",
         rowId: "row_123",
         annotation: {
@@ -56,15 +68,121 @@ describe("ExperimentDataAnnotationsController", () => {
         },
       };
 
-      // TODO: It results 400 instead or 201, need to investigate
       // Send the request
       const response: SuperTestResponse<AnnotationRowsAffected> = await testApp
         .post(path)
         .withAuth(testUserId)
-        .send(addAnnotationData)
+        .send(addAnnotationsData)
         .expect(StatusCodes.CREATED);
 
       expect(response.body.rowsAffected).toBe(1);
+    });
+  });
+
+  describe("addAnnotationsBulk", () => {
+    it("Should add multiple annotation to a single row", async () => {
+      vi.spyOn(addAnnotationsUseCase, "execute").mockResolvedValue(success({ rowsAffected: 3 }));
+
+      // Construct the path
+      const path = testApp.resolvePath(contract.experiments.addAnnotationsBulk.path, {
+        id: "06a9ac24-b888-4a97-a883-16354d2cf63c",
+      });
+
+      // Create the request body
+      const addAnnotationsData: AddAnnotationsBulkBody = {
+        tableName: "data_table_1",
+        rowIds: ["row_123", "row_456", "row_789"],
+        annotation: {
+          type: "comment",
+          content: {
+            text: "This is a test comment",
+          },
+        },
+      };
+
+      // Send the request
+      const response: SuperTestResponse<AnnotationRowsAffected> = await testApp
+        .post(path)
+        .withAuth(testUserId)
+        .send(addAnnotationsData)
+        .expect(StatusCodes.CREATED);
+
+      expect(response.body.rowsAffected).toBe(3);
+    });
+  });
+
+  describe("updateAnnotation", () => {
+    it("Should update an annotation on a single row", async () => {
+      vi.spyOn(updateAnnotationUseCase, "execute").mockResolvedValue(success({ rowsAffected: 1 }));
+
+      // Construct the path
+      const path = testApp.resolvePath(contract.experiments.updateAnnotation.path, {
+        id: "06a9ac24-b888-4a97-a883-16354d2cf63c",
+        annotationId: "9f244bae-22d7-48c1-9459-b02a6846cea8",
+      });
+
+      // Create the request body
+      const updateAnnotationData: UpdateAnnotationBody = {
+        content: {
+          text: "This is the modified comment",
+        },
+      };
+
+      // Send the request
+      const response: SuperTestResponse<AnnotationRowsAffected> = await testApp
+        .patch(path)
+        .withAuth(testUserId)
+        .send(updateAnnotationData)
+        .expect(StatusCodes.NO_CONTENT);
+
+      expect(response.body.rowsAffected).toBe(undefined);
+    });
+  });
+
+  describe("deleteAnnotation", () => {
+    it("Should delete an annotation from a single row", async () => {
+      vi.spyOn(deleteAnnotationsUseCase, "execute").mockResolvedValue(success({ rowsAffected: 1 }));
+
+      // Construct the path
+      const path = testApp.resolvePath(contract.experiments.deleteAnnotation.path, {
+        id: "06a9ac24-b888-4a97-a883-16354d2cf63c",
+        annotationId: "9f244bae-22d7-48c1-9459-b02a6846cea8",
+      });
+
+      // Send the request
+      const response: SuperTestResponse<AnnotationRowsAffected> = await testApp
+        .delete(path)
+        .withAuth(testUserId)
+        .expect(StatusCodes.NO_CONTENT);
+
+      expect(response.body.rowsAffected).toBe(undefined);
+    });
+  });
+
+  describe("deleteAnnotationsBulk", () => {
+    it("Should delete all comments annotations from multiple rows", async () => {
+      vi.spyOn(deleteAnnotationsUseCase, "execute").mockResolvedValue(success({ rowsAffected: 5 }));
+
+      // Construct the path
+      const path = testApp.resolvePath(contract.experiments.deleteAnnotationsBulk.path, {
+        id: "06a9ac24-b888-4a97-a883-16354d2cf63c",
+      });
+
+      // Create the request body
+      const deleteAnnotationsBulkData: DeleteAnnotationsBulkBody = {
+        tableName: "data_table_1",
+        rowIds: ["row_123", "row_456"],
+        type: "comment",
+      };
+
+      // Send the request
+      const response: SuperTestResponse<AnnotationRowsAffected> = await testApp
+        .post(path)
+        .withAuth(testUserId)
+        .send(deleteAnnotationsBulkData)
+        .expect(StatusCodes.NO_CONTENT);
+
+      expect(response.body.rowsAffected).toBe(undefined);
     });
   });
 });
