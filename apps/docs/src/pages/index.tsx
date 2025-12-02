75e169429291df431fc1da5c065c1843def038f6
Michiel Schutler <michiel@info.nl>
2025-12-02 15:59:43 +0100

    chore(docs): Changed homepage into redirect to overview page

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,29 +1,33 @@
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import SearchBar from "@theme/SearchBar";
+import { Redirect } from "@docusaurus/router";
+//import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+//import Layout from "@theme/Layout";
+// import SearchBar from "@theme/SearchBar";
 import React from "react";
 
-import styles from "./index.module.css";
+//import styles from "./index.module.css";
 
-function HomepageHeader(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={styles.hero}>
-      <div className={styles.container}>
-        <div className={styles.searchContainer}>
-          <SearchBar />
-        </div>
-      </div>
-    </header>
-  );
-}
+// function HomepageHeader(): JSX.Element {
+//   const { siteConfig } = useDocusaurusContext();
+//   return (
+//     <header className={styles.hero}>
+//       <div className={styles.container}>
+//         {/*<div className={styles.searchContainer}>*/}
+//         {/*  <SearchBar />*/}
+//         {/*</div>*/}
+//       </div>
+//     </header>
+//   );
+// }
 
-export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout title={siteConfig.title} description="Your site description">
-      <HomepageHeader />
-      <main>{/* Other homepage content */}</main>
-    </Layout>
-  );
+// export default function Home(): JSX.Element {
+//   const { siteConfig } = useDocusaurusContext();
+//   return (
+//     <Layout title={siteConfig.title} description="Your site description">
+//       <HomepageHeader />
+//       <main>{/* Other homepage content */}</main>
+//     </Layout>
+//   );
+// }
+export default function HomeRedirect() {
+  return <Redirect to="/docs/introduction/overview" />;
 }
