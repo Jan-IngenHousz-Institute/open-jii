da447fb7d4bc9e588f1db780a01b8517741d5f0a
Michiel Schutler <michiel@info.nl>
2025-12-03 10:59:00 +0100

    chore(docs): Fixed deprecated config option

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -21,7 +21,6 @@ const config: Config = {
   projectName: "openJII", // Usually your repo name.
 
   onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -33,6 +32,9 @@ const config: Config = {
 
   markdown: {
     mermaid: true,
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+    },
   },
   themes: ["@docusaurus/theme-mermaid"],
 
