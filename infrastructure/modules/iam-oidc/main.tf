89bb62dc003d7cc90852db61dbad00e73eb50948
Blagoj <blagoj@info.nl>
2025-12-08 17:26:31 +0100

    Modify OIDC role permission

--- infrastructure/modules/iam-oidc/main.tf
+++ infrastructure/modules/iam-oidc/main.tf
@@ -1,5 +1,34 @@
 data "aws_caller_identity" "current" {}
 
+# AWS Managed Policies for Full Access to all resources
+locals {
+  aws_managed_policies = [
+    "AmazonEC2FullAccess",
+    "AmazonVPCFullAccess",
+    "AmazonS3FullAccess",
+    "CloudFrontFullAccess",
+    "AmazonTimestreamFullAccess",
+    "AmazonKinesisFullAccess",
+    "AWSIoTFullAccess",
+    "IAMFullAccess",
+    "AmazonDynamoDBFullAccess",
+    "AmazonCognitoPowerUser",
+    "AmazonRDSFullAccess",
+    "CloudWatchFullAccess",
+    "AmazonEC2ContainerRegistryFullAccess",
+    "AmazonECS_FullAccess",
+    "AWSLambda_FullAccess",
+    "AmazonSQSFullAccess",
+    "AmazonRoute53FullAccess",
+    "SecretsManagerReadWrite",
+    "AmazonSESFullAccess",
+    "AmazonSSMFullAccess",
+    "AWSWAFFullAccess",
+    "AmazonLocationFullAccess",
+    "ElasticLoadBalancingFullAccess",
+  ]
+}
+
 resource "aws_iam_openid_connect_provider" "github" {
   url             = var.oidc_provider_url
   client_id_list  = var.client_id_list
@@ -34,186 +63,8 @@ resource "aws_iam_role" "oidc_role" {
   })
 }
 
-resource "aws_iam_policy" "oidc_role_policy" {
-  name        = "GitHubActionsAccess"
-  description = "Policy for GitHub Actions OIDC role with permissions for VPC, S3, CloudFront, Timestream, Kinesis, IoT, and IAM actions."
-  policy = jsonencode({
-    "Statement" : [
-      {
-        "Action" : [
-          "ec2:CreateVpc",
-          "ec2:DeleteVpc",
-          "ec2:DescribeVpcs",
-          "ec2:CreateSubnet",
-          "ec2:DeleteSubnet",
-          "ec2:DescribeSubnets",
-          "ec2:CreateInternetGateway",
-          "ec2:AttachInternetGateway",
-          "ec2:DeleteInternetGateway",
-          "ec2:DescribeInternetGateways",
-          "ec2:DescribeSecurityGroups",
-          "ec2:DescribeRouteTables",
-          "ec2:DescribeAddressesAttribute",
-          "ec2:CreateRouteTable",
-          "ec2:AssociateRouteTable",
-          "ec2:DisassociateRouteTable",
-          "ec2:DeleteRouteTable",
-          "ec2:CreateVpcEndpoint",
-          "ec2:DescribeVpcEndpoints",
-          "ec2:DeleteVpcEndpoint",
-          "ec2:DescribeAvailabilityZones",
-          "ec2:DescribeVpcAttribute",
-          "ec2:DescribeAddresses",
-          "ec2:DescribeNatGateways",
-          "ec2:DescribePrefixLists",
-          "ec2:DescribeNetworkInterfaces"
-        ],
-        "Effect" : "Allow",
-        "Resource" : "*",
-        "Sid" : "EC2Permissions"
-      },
-      {
-        "Action" : [
-          "s3:CreateBucket",
-          "s3:DeleteBucket",
-          "s3:ListBucket",
-          "s3:GetBucketPolicy",
-          "s3:PutBucketPolicy",
-          "s3:DeleteBucketPolicy",
-          "s3:PutBucketAcl",
-          "s3:GetBucketAcl",
-          "s3:DeleteObject",
-          "s3:PutObject",
-          "s3:GetObject",
-          "s3:GetBucketCORS",
-          "s3:GetBucketWebsite",
-          "s3:GetBucketVersioning",
-          "s3:GetAccelerateConfiguration",
-          "s3:GetBucketRequestPayment",
-          "s3:GetBucketLogging",
-          "s3:GetLifecycleConfiguration",
-          "s3:GetReplicationConfiguration",
-          "s3:GetEncryptionConfiguration",
-          "s3:GetBucketObjectLockConfiguration",
-          "s3:GetBucketTagging",
-          "s3:GetBucketOwnershipControls",
-          "s3:GetBucketPublicAccessBlock"
-        ],
-        "Effect" : "Allow",
-        "Resource" : "*",
-        "Sid" : "S3Permissions"
-      },
-      {
-        "Action" : [
-          "cloudfront:CreateDistribution",
-          "cloudfront:UpdateDistribution",
-          "cloudfront:DeleteDistribution",
-          "cloudfront:GetDistribution",
-          "cloudfront:ListDistributions",
-          "cloudfront:GetOriginAccessControl",
-          "cloudfront:ListTagsForResource"
-        ],
-        "Effect" : "Allow",
-        "Resource" : "*",
-        "Sid" : "CloudFrontPermissions"
-      },
-      {
-        "Action" : [
-          "timestream:CreateDatabase",
-          "timestream:DescribeDatabase",
-          "timestream:DeleteDatabase",
-          "timestream:CreateTable",
-          "timestream:DescribeTable",
-          "timestream:DeleteTable",
-          "timestream:DescribeEndpoints",
-          "timestream:ListTagsForResource"
-        ],
-        "Effect" : "Allow",
-        "Resource" : "*",
-        "Sid" : "TimestreamPermissions"
-      },
-      {
-        "Action" : [
-          "kinesis:CreateStream",
-          "kinesis:DescribeStream",
-          "kinesis:DeleteStream",
-          "kinesis:ListStreams",
-          "kinesis:PutRecord",
-          "kinesis:PutRecords",
-          "kinesis:DescribeStreamSummary",
-          "kinesis:ListTagsForStream"
-        ],
-        "Effect" : "Allow",
-        "Resource" : "*",
-        "Sid" : "KinesisPermissions"
-      },
-      {
-        "Action" : [
-          "iot:CreateThing",
-          "iot:DeleteThing",
-          "iot:DescribeThing",
-          "iot:UpdateThing",
-          "iot:ListThings",
-          "iot:GetPolicy",
-          "iot:ListTagsForResource",
-          "iot:GetV2LoggingOptions",
-          "iot:ListTopicRules",
-          "iot:GetTopicRule"
-        ],
-        "Effect" : "Allow",
-        "Resource" : "*",
-        "Sid" : "IoTCorePermissions"
-      },
-      {
-        "Action" : [
-          "iam:CreateRole",
-          "iam:DeleteRole",
-          "iam:AttachRolePolicy",
-          "iam:PutRolePolicy",
-          "iam:PassRole",
-          "iam:GetPolicy",
-          "iam:GetRole",
-          "iam:GetOpenIDConnectProvider",
-          "iam:GetPolicyVersion",
-          "iam:ListRolePolicies",
-          "iam:GetRolePolicy",
-          "iam:ListAttachedRolePolicies"
-        ],
-        "Effect" : "Allow",
-        "Resource" : "*",
-        "Sid" : "IAMPermissionsForIoTAndDatabricks"
-      },
-      {
-        "Action" : [
-          "dynamodb:GetItem",
-          "dynamodb:PutItem",
-          "dynamodb:DescribeTable",
-          "dynamodb:DeleteItem",
-          "dynamodb:DescribeContinuousBackups",
-          "dynamodb:DescribeTimeToLive",
-          "dynamodb:ListTagsOfResource"
-        ],
-        "Effect" : "Allow",
-        "Resource" : "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/terraform-state-lock",
-        "Sid" : "DynamoDBs3lock"
-      },
-      {
-        "Action" : [
-          "cognito-identity:DescribeIdentityPool",
-          "cognito-identity:ListIdentityPools",
-          "cognito-identity:GetIdentityPoolRoles",
-          "cognito-identity:LookupDeveloperIdentity"
-        ],
-        "Effect" : "Allow",
-        "Resource" : "*",
-        "Sid" : "CognitoPools"
-      }
-    ],
-    "Version" : "2012-10-17"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "oidc_role_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "oidc_managed_policies" {
+  for_each = toset(local.aws_managed_policies)
   role       = aws_iam_role.oidc_role.name
-  policy_arn = aws_iam_policy.oidc_role_policy.arn
+  policy_arn = "arn:aws:iam::aws:policy/${each.value}"
 }
