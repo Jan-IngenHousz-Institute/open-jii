95cfa9e178af05b398058847a2974d8ca8281dda
Blagoj <blagoj@info.nl>
2026-01-29 16:52:59 +0100

    Change rate limit for waf rule

--- infrastructure/env/prod/main.tf
+++ infrastructure/env/prod/main.tf
@@ -842,7 +842,7 @@ module "opennext_waf" {
 
   service_name       = "opennext"
   environment        = var.environment
-  rate_limit         = 500
+  rate_limit         = 5000
   log_retention_days = 30
 
   tags = {
