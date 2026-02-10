d50756b14f895dbc5a4106145e2f6c029b4f3d2b
Blagoj <blagoj@info.nl>
2026-02-10 15:24:28 +0100

    Create cloudtrail module

---
+++ infrastructure/modules/cloudtrail/main.tf
@@ -0,0 +1,72 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  trail_name = "${var.project_name}-${var.environment}-cloudtrail"
+}
+
+resource "aws_cloudtrail" "cloud_trail" {
+  name                          = local.trail_name
+  s3_bucket_name                = aws_s3_bucket.cloud_trail_bucket.id
+  include_global_service_events = true
+  is_multi_region_trail         = true
+
+  depends_on = [aws_s3_bucket_policy.cloud_trail_bucket_policy]
+}
+
+resource "aws_s3_bucket" "cloud_trail_bucket" {
+  bucket        = "${var.project_name}-${var.environment}-trail-bucket"
+  force_destroy = true
+}
+
+data "aws_iam_policy_document" "cloud_trail_bucket_policy" {
+  statement {
+    sid    = "AWSCloudTrailAclCheck"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:GetBucketAcl"]
+    resources = [aws_s3_bucket.cloud_trail_bucket.arn]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = ["arn:aws:cloudtrail:${var.region}:${data.aws_caller_identity.current.account_id}:trail/${local.trail_name}"]
+    }
+  }
+
+  statement {
+    sid    = "AWSCloudTrailWrite"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.cloud_trail_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = ["arn:aws:cloudtrail:${var.region}:${data.aws_caller_identity.current.account_id}:trail/${local.trail_name}"]
+    }
+  }
+
+  depends_on = [aws_s3_bucket.cloud_trail_bucket]
+}
+
+resource "aws_s3_bucket_policy" "cloud_trail_bucket_policy" {
+  bucket = aws_s3_bucket.cloud_trail_bucket.id
+  policy = data.aws_iam_policy_document.cloud_trail_bucket_policy.json
+
+  depends_on = [aws_s3_bucket.cloud_trail_bucket, data.aws_iam_policy_document.cloud_trail_bucket_policy]
+}
