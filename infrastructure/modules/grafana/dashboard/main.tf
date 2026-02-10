81cd416ed56182838ac7276770bd0e9c93f61a68
Blagoj <blagoj@info.nl>
2026-02-10 16:18:36 +0100

    Add 500 error alert rule

--- infrastructure/modules/grafana/dashboard/main.tf
+++ infrastructure/modules/grafana/dashboard/main.tf
@@ -117,6 +117,7 @@ resource "grafana_rule_group" "backend_alerts" {
   name             = "Backend API Alerts"
   folder_uid       = grafana_folder.folder.uid
   interval_seconds = 60
+  # disable_provenance = true
 
   rule {
     name      = "Backend High CPU Usage"
@@ -263,6 +264,85 @@ EOT
       service  = "backend"
     }
   }
+
+  #   rule {
+  #     name      = "Backend High 5xx Error Rate"
+  #     condition = "C"
+
+  #     data {
+  #       ref_id         = "A"
+  #       query_type     = ""
+  #       datasource_uid = grafana_data_source.cloudwatch_source.uid
+
+  #       model = jsonencode({
+  #         refId            = "A"
+  #         region           = var.aws_region
+  #         namespace        = "AWS/ApplicationELB"
+  #         metricName       = "HTTPCode_Target_5XX_Count"
+  #         statistic        = "Sum"
+  #         period           = "300"
+  #         matchExact       = true
+  #         metricEditorMode = 0
+  #         metricQueryType  = 0
+  #         queryMode        = "Metrics"
+  #         id               = "m1"
+  #         expression       = "FILL(m1, 0)"
+  #         dimensions = {
+  #           LoadBalancer = [local.dashboard_vars.load_balancer_dimension]
+  #         }
+  #       })
+
+  #       relative_time_range {
+  #         from = 300
+  #         to   = 0
+  #       }
+  #     }
+
+  #     data {
+  #       ref_id         = "B"
+  #       query_type     = ""
+  #       datasource_uid = "__expr__"
+
+  #       model = <<EOT
+  # {"conditions":[{"evaluator":{"params":[0,0],"type":"gt"},"operator":{"type":"and"},"query":{"params":["A"]},"reducer":{"params":[],"type":"sum"},"type":"query"}],"datasource":{"name":"Expression","type":"__expr__","uid":"__expr__"},"expression":"A","hide":false,"intervalMs":1000,"maxDataPoints":43200,"reducer":"sum","refId":"B","type":"reduce"}
+  # EOT
+
+  #       relative_time_range {
+  #         from = 0
+  #         to   = 0
+  #       }
+  #     }
+
+  #     data {
+  #       ref_id         = "C"
+  #       query_type     = ""
+  #       datasource_uid = "__expr__"
+
+  #       model = jsonencode({
+  #         expression = "$B > 5"
+  #         type       = "math"
+  #         refId      = "C"
+  #       })
+
+  #       relative_time_range {
+  #         from = 0
+  #         to   = 0
+  #       }
+  #     }
+
+  #     no_data_state  = "OK"
+  #     exec_err_state = "OK"
+  #     for            = "5m"
+
+  #     annotations = {
+  #       description = "Backend is returning 5xx errors"
+  #       summary     = "5xx errors detected on backend service"
+  #     }
+  #     labels = {
+  #       severity = "critical"
+  #       service  = "backend"
+  #     }
+  #   }
 }
 
 # CloudFront Alerts
