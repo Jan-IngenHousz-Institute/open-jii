1bf006f573f47adecde9fa1752ade116a4bec963
Sander <mail@sanderboer.nl>
2026-02-24 14:34:48 +0100

    Simplifies metadata table component
    Removes unused exports and types from the metadata table component.
    
    These exports are not currently in use and their removal simplifies the component's API and reduces the bundle size.

--- apps/web/components/metadata-table/index.ts
+++ apps/web/components/metadata-table/index.ts
@@ -1,11 +1,8 @@
 export { MetadataProvider, useMetadata } from "./metadata-context";
 export { MetadataTable } from "./metadata-table";
-export { EditableCell } from "./editable-cell";
 export type {
   MetadataColumn,
   MetadataRow,
   MetadataTableState,
-  MetadataImportConfig,
   MetadataContextValue,
 } from "./types";
-export { parseDelimitedText, parseFile, parseClipboard } from "./utils/parse-metadata-import";
