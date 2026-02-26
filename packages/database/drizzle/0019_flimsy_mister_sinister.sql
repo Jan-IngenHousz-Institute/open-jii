822901d7d1dd261f51bb373b5679b6d12aa9c744
petar-INFO <petar@info.nl>
2026-02-26 13:18:11 +0100

    fix(db): generates and applies migration to remove provisioning status

--- packages/database/drizzle/0019_flimsy_mister_sinister.sql
+++ packages/database/drizzle/0019_flimsy_mister_sinister.sql
@@ -1,5 +1,6 @@
 ALTER TABLE "experiments" ALTER COLUMN "status" SET DATA TYPE text;--> statement-breakpoint
 ALTER TABLE "experiments" ALTER COLUMN "status" SET DEFAULT 'active'::text;--> statement-breakpoint
+UPDATE "experiments" SET "status" = 'active' WHERE "status" IN ('provisioning', 'provisioning_failed');--> statement-breakpoint
 DROP TYPE "public"."experiment_status";--> statement-breakpoint
 CREATE TYPE "public"."experiment_status" AS ENUM('active', 'stale', 'archived', 'published');--> statement-breakpoint
 ALTER TABLE "experiments" ALTER COLUMN "status" SET DEFAULT 'active'::"public"."experiment_status";--> statement-breakpoint
