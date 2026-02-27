8b1f830232a17d5fa042886b267e27def25f3ba0
petar-INFO <petar@info.nl>
2026-02-27 15:27:58 +0100

    fix

--- infrastructure/env/prod/main.tf
+++ infrastructure/env/prod/main.tf
@@ -197,6 +197,9 @@ module "node_cluster_policy" {
       whl = "/Workspace/Shared/.bundle/open-jii/${var.environment}/artifacts/.internal/enrich-0.1.0-py3-none-any.whl"
     },
     {
+      whl = "/Workspace/Shared/.bundle/open-jii/${var.environment}/artifacts/.internal/openjii-0.1.0-py3-none-any.whl"
+    },
+    {
       pypi = {
         package = "mini-racer==0.12.4"
       }
