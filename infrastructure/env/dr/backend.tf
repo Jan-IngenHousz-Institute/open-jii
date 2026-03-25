ced7dcd24733351bde5215ef0142317391ee332b
Blagoj <blagoj@info.nl>
2026-03-25 14:02:00 +0100

    Update

--- infrastructure/env/dr/backend.tf
+++ infrastructure/env/dr/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "open-jii-terraform-state-dr"
-#     key            = "terraform.tfstate"
-#     region         = "eu-west-1"
-#     dynamodb_table = "terraform-state-lock"
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "open-jii-terraform-state-dr"
+    key            = "terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
