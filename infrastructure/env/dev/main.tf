b9c61a9d2ee5fe70ea764cef40d0f9a303f07818
petar-INFO <petar@info.nl>
2026-03-26 10:49:17 +0100

    adjustments

--- infrastructure/env/dev/main.tf
+++ infrastructure/env/dev/main.tf
@@ -404,6 +404,9 @@ module "databricks_catalog" {
   depends_on = [module.node_service_principal]
 }
 
+// This resource is imported by hand, databricks_pipeline creates the schema and tables,
+// but we want to manage permissions on the catalog level from Terraform,
+// so we need to import the catalog resource after the pipeline is created and then manage permissions from Terraform 
 module "centrum_schema" {
   source = "../../modules/databricks/schema"
 
--- infrastructure/modules/databricks/pipeline/main.tf
+++ infrastructure/modules/databricks/pipeline/main.tf
@@ -21,6 +21,10 @@ resource "databricks_pipeline" "this" {
 
   catalog = var.catalog_name
 
+  lifecycle {
+    prevent_destroy = true
+  }
+
   # Support for notebook libraries
   dynamic "library" {
     for_each = var.notebook_paths
