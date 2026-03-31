3cbf6c8bafa36e32265c1f7ca6e4ff5b266f11b6
xiduzo <mail@sanderboer.nl>
2026-03-31 14:07:46 +0200

    ts-magic

--- apps/web/hooks/macro/useMacroCreate/useMacroCreate.ts
+++ apps/web/hooks/macro/useMacroCreate/useMacroCreate.ts
@@ -1,21 +1,23 @@
 import { isContractError, tsr } from "../../../lib/tsr";
 import type { TsRestMutationOptions, TsrRoute } from "../../../lib/tsr";
 
+const route = tsr.macros.createMacro;
+
 export type UseMacroCreateOptions = TsRestMutationOptions<
-  TsrRoute<typeof tsr.macros.createMacro>,
+  TsrRoute<typeof route>,
   "onSuccess" | "onError"
 >;
 
 export function useMacroCreate(options?: UseMacroCreateOptions) {
   const queryClient = tsr.useQueryClient();
 
-  return tsr.macros.createMacro.useMutation({
+  return route.useMutation({
     onSuccess: (...args) => {
       void queryClient.invalidateQueries({ queryKey: ["macros"] });
       options?.onSuccess?.(...args);
     },
     onError: (error, ...rest) => {
-      if (!isContractError(tsr.macros.createMacro, error)) return;
+      if (!isContractError(error)) return;
       options?.onError?.(error, ...rest);
     },
   });
