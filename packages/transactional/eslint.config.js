7e8684c8ed76da6900ee6b972d16766b04bc9dcd
Vlad Stoenescu <vlad@info.nl>
2026-04-08 15:46:46 +0200

    fix

--- packages/transactional/eslint.config.js
+++ packages/transactional/eslint.config.js
@@ -6,12 +6,4 @@ export default [
     ignores: ["dist/**"],
   },
   ...baseConfig,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: false,
-        project: "./tsconfig.eslint.json",
-      },
-    },
-  },
 ];
