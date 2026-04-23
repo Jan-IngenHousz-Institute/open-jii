76ad07bde096b7e1ba83c0dc7fc7519fdd09fd94
Blagoj <blagoj@info.nl>
2026-04-23 13:35:16 +0200

    feat(monitoring): Fix user registrations dashboard and automate Lambda dependency installation

--- infrastructure/modules/grafana/metrics-publisher/main.tf
+++ infrastructure/modules/grafana/metrics-publisher/main.tf
@@ -1,10 +1,23 @@
 data "aws_caller_identity" "current" {}
 
+resource "null_resource" "npm_install" {
+  triggers = {
+    package_json = filemd5("${path.module}/lambda/package.json")
+  }
+
+  provisioner "local-exec" {
+    command     = "npm install --omit=dev"
+    working_dir = "${path.module}/lambda"
+  }
+}
+
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "${path.module}/lambda"
   output_path = "${path.module}/lambda/metrics_publisher.zip"
   excludes    = ["metrics_publisher.zip"]
+
+  depends_on = [null_resource.npm_install]
 }
 
 # -------------------------
