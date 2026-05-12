7d680957a83e55bffbf6317d5d0ae7f4d31a054a
Blagoj <blagoj@info.nl>
2026-05-12 13:09:03 +0200

    Add missing output

--- infrastructure/modules/iot-core/outputs.tf
+++ infrastructure/modules/iot-core/outputs.tf
@@ -8,3 +8,8 @@ output "iot_kinesis_role_arn" {
   value       = aws_iam_role.iot_kinesis_role.arn
 }
 
+output "backend_s3_presign_policy_arn" {
+  description = "ARN of the IAM policy that allows the backend ECS task role to generate pre-signed S3 PutObject URLs for large IoT payloads"
+  value       = aws_iam_policy.backend_s3_presign.arn
+}
+
