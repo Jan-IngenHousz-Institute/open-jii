25ce954c2b0b2b8bead8709f3beca38d9011995b
Blagoj <blagoj@info.nl>
2026-08-21 17:19:44 +0200

    feat(infra): remove sandbox VPC, Databricks workspace, and Unity Catalog (migrated to jii-infra)

--- infrastructure/env/sandbox/versions.tf
+++ infrastructure/env/sandbox/versions.tf
@@ -2,11 +2,6 @@ terraform {
   required_version = ">= 1.5.6"
 
   required_providers {
-    databricks = {
-      source  = "databricks/databricks"
-      version = ">=1.13.0"
-    }
-
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.0"
@@ -21,21 +16,4 @@ provider "aws" {
 provider "aws" {
   alias  = "dr"
   region = "eu-west-1"
-}
-
-provider "databricks" {
-  alias         = "mws"
-  host          = "https://accounts.cloud.databricks.com"
-  account_id    = var.databricks_account_id
-  client_id     = var.databricks_client_id
-  client_secret = var.databricks_client_secret
-  auth_type     = "oauth-m2m"
-}
-
-provider "databricks" {
-  alias         = "workspace"
-  host          = var.databricks_host
-  client_id     = var.databricks_client_id
-  client_secret = var.databricks_client_secret
-  auth_type     = "oauth-m2m"
 }
