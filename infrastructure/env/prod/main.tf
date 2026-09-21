85a8f689a823bf6c156281b543b98fad59a7baf4
Petar <petar@info.nl>
2026-09-21 10:00:07 +0200

    chore(infra): extend the prod Kinesis ingest stream retention to 3 days (#2108)
    ## Type of PR (check all applicable)
    
    - [ ] Feature
    - [ ] Bug Fix
    - [ ] Refactor
    - [ ] Documentation Update
    - [ ] Performance Improvement
    - [ ] Test Update
    - [x] Chore
    - [ ] Other (please describe)
    
    ## Description
    
    The prod data-ingest Kinesis stream ran on the default 24-hour
    retention, which is also the window in which measurements can be
    replayed from the stream before recovery has to fall back to the S3
    archive. This sets it to 72 hours, so a pipeline outage or a delayed
    reprocess has three days of headroom on the stream itself rather than
    one.

--- infrastructure/env/prod/main.tf
+++ infrastructure/env/prod/main.tf
@@ -73,6 +73,8 @@ module "kinesis" {
   source      = "../../modules/kinesis"
   stream_name = "open-jii-${var.environment}-data-ingest-stream"
 
+  retention_period_hours = 72
+
   workspace_kinesis_credential_id = var.kinesis_credential_id
 }
 
